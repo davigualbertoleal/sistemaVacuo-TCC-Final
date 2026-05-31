10ed647984a1f3e782d6bf3a18d75242cf9725d5
davigualbertoleal <davigualbertoleal41@gmail.com>
2026-05-31 01:51:24 -0300

    adição de dependênciasAWS e modificações para aceitação do ambiente cloud

--- apiSistemaVacuo/apiSistemaVacuo/Program.cs
+++ apiSistemaVacuo/apiSistemaVacuo/Program.cs
@@ -1,10 +1,8 @@
 using MySql.Data.MySqlClient;
-using System.Collections.Generic;
-using System.Text.Json;
+using Amazon.S3;  // <- adiciona
 
 var builder = WebApplication.CreateBuilder(args);
 
-// --- ADICIONA SERVIÇOS ---
 builder.Services.AddControllers();
 builder.Services.AddCors(options =>
 {
@@ -16,19 +14,19 @@ builder.Services.AddCors(options =>
     });
 });
 
-// --- CONEXÃO MYSQL ---
+// <- adiciona essas duas
+builder.Services.AddDefaultAWSOptions(builder.Configuration.GetAWSOptions());
+builder.Services.AddAWSService<IAmazonS3>();
+
 var connectionString = "Server=localhost;Database=ProcessoVacuo;Uid=root;Pwd=;";
 builder.Services.AddScoped<MySqlConnection>(_ => new MySqlConnection(connectionString));
 
 var app = builder.Build();
 
-// --- MIDDLEWARE ---
 app.UseRouting();
 app.UseCors("AllowAll");
-
 app.MapControllers();
 
-// --- HEALTH CHECK ---
 app.MapGet("/health", () => Results.Ok(new { status = "API rodando!", timestamp = DateTime.UtcNow }));
 
 app.Run("http://0.0.0.0:5000");
